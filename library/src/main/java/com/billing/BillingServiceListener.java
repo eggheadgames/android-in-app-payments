2b38de99bc1c793a1a283003ad9735cafe73a131
Yuriy Diachenko <handsome.ups@gmail.com>
2016-05-14 16:12:28 +0300

    changed library listener to be an abstract class

--- library/src/main/java/com/billing/BillingServiceListener.java
+++ library/src/main/java/com/billing/BillingServiceListener.java
@@ -2,41 +2,45 @@ package com.billing;
 
 import java.util.Map;
 
-public interface BillingServiceListener {
+public abstract class BillingServiceListener {
 
     /**
      * Callback will be triggered upon obtaining information about product prices
      *
      * @param iapKeyPrices - a map with available products
      */
-    void onPricesUpdated(Map<String, String> iapKeyPrices);
+    public void onPricesUpdated(Map<String, String> iapKeyPrices) {
+    }
 
     /**
      * Callback will be triggered when a product purchased successfully
      *
      * @param sku - specificator of owned product
      */
-    void onProductPurchased(String sku);
+    public void onProductPurchased(String sku) {
+    }
 
     /**
      * Callback will be triggered upon owned products restore
      *
      * @param sku - specificator of owned product
      */
-    void onProductRestored(String sku);
+    public void onProductRestored(String sku) {
+    }
 
     /**
      * Callback will be triggered upon owned subscription restore
      *
      * @param sku - specificator of owned subscription
      */
-    void onSubscriptionRestored(String sku);
+    public void onSubscriptionRestored(String sku) {
+    }
 
     /**
      * Callback will be triggered when a subscription purchased successfully
      *
      * @param sku - specificator of purchased subscription
      */
-    void onSubscriptionPurchased(String sku);
-
+    public void onSubscriptionPurchased(String sku) {
+    }
 }
